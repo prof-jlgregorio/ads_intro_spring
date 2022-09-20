0507303d78a89cbb093ae7f0456aeeca4acf2dd1
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2022-09-20 10:15:20 -0300

    First Commit!

---
+++ src/main/java/br/com/ads/IntroApp/IntroAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.ads.IntroApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ads/IntroApp/IntroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ads.IntroApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
