a23768c4d2e608b680b6970968a5c0e8cedb8cf9
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2022-09-27 10:12:57 -0300

    Client CRUD Operations are OK!

--- src/main/java/br/com/ads/IntroApp/controller/ClientController.java
+++ src/main/java/br/com/ads/IntroApp/controller/ClientController.java
@@ -3,6 +3,7 @@ package br.com.ads.IntroApp.controller;
 import br.com.ads.IntroApp.model.ClientModel;
 import br.com.ads.IntroApp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,6 +31,20 @@ public class ClientController {
         return service.save(model);
     }
 
+    @PutMapping
+    public ClientModel update(@RequestBody ClientModel model){
+        return service.update(model);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id){
+        Optional<ClientModel> found = service.findById(id);
+        if(found.isPresent()){
+          service.delete(id);
+          return ResponseEntity.ok().build();
+        } else {
+            return null;
+        }
+    }
 
 }
--- src/main/java/br/com/ads/IntroApp/model/ClientModel.java
+++ src/main/java/br/com/ads/IntroApp/model/ClientModel.java
@@ -1,23 +1,29 @@
 package br.com.ads.IntroApp.model;
-import lombok.Data;
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "clients")
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter @Getter
     private long id;
 
     @Column(name = "name", nullable = false, length = 50)
+    @Setter @Getter
     private String name;
 
     @Column(nullable = false, length = 1)
+    @Getter @Setter
     private String gender;
 
     @Column(nullable = false, length = 50)
+    @Getter @Setter
     private String city;
 
 
